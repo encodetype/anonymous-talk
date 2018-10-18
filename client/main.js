2fe2267082d7e22c7b74019814d27bab98d36cb6
CarlGauss <staff@Staffs-MacBook-Pro-6.local>
2018-10-19 00:12:47 +0700

    updated

--- client/main.js
+++ client/main.js
@@ -1,3 +1,38 @@
+import moment from 'moment';
+import uuid from 'uuid';
+import { ReactiveVar } from 'meteor/reactive-var'
+import { Session } from 'meteor/session'
+
+import './components/chat';
+import './components/input';
+
 import 'bulma/css/bulma.css'
 
 import './main.html';
+
+uid = uuid();
+
+Session.set('uid',uid);
+
+Chats = new Mongo.Collection('chats');
+
+Template.Chat_Timer.onCreated(function(){
+  self = this;
+
+  self.timer = new ReactiveVar(null);
+});
+
+Template.Chat_Timer.onRendered(function(){
+  self = this;
+
+  Meteor.setInterval(function(){
+    //console.log(self.timer)
+    self.timer.set(new Date());
+  },1000);
+});
+
+Template.Chat_Timer.helpers({
+  timer(){
+    return Template.instance().timer.get()
+  }
+})
--- server/main.js
+++ server/main.js
@@ -1,5 +1,19 @@
 import { Meteor } from 'meteor/meteor';
 
+
+
 Meteor.startup(() => {
   // code to run on server at startup
+  Chats = new Mongo.Collection('chats');
+
+  Meteor.publish('cur_chat',()=>{
+    return Chats.find();
+  });
+
+  Meteor.methods({
+    send_message(uid,msg){
+      Chats.insert({ user:uid,msg:msg,created:new Date() });
+    }
+  });
+
 });
